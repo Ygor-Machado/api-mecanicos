a88d623c401e7b051ade338b1a69516715bc5663
Ygor-Machado <ygaounderground011@gmail.com>
2025-03-28 20:08:39 -0300

    feat: Inicia parte autenticação

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->middleware(['throttle:10,1'])
+    ->name('login');
